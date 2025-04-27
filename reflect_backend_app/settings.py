97c9a117e5cc507fbaea86ce1dbc312307010d02
roman <r3wqaxz@yandex.ru>
2025-04-27 19:16:59 +0300

    Hotfix: add static dirs for Docker

--- reflect_backend_app/settings.py
+++ reflect_backend_app/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os.path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -145,8 +145,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Настройки для статических файлов
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Куда собирать статику
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Где искать статику
 
+# Настройки для медиа файлов
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 

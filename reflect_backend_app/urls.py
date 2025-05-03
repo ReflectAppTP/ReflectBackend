9bd1bb7a5b83d54a12ef893653603aeb1be18268
roman <r3wqaxz@yandex.ru>
2025-05-03 08:34:11 +0300

    Hotfix api token urls

--- reflect_backend_app/urls.py
+++ reflect_backend_app/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/authReflect/', include('api.authReflect.urls')),
     path('api/emotions/', include('api.emotions.urls')),
+path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]

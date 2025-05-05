83ff11f64d5d11ed9771537a31258aebe0e3dd2a
roman <r3wqaxz@yandex.ru>
2025-05-05 23:46:03 +0300

    Hotfix timezone

--- reflect_backend_app/settings.py
+++ reflect_backend_app/settings.py
@@ -16,6 +16,8 @@ import os.path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+TIME_ZONE = 'Europe/Moscow'
+USE_TZ = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/

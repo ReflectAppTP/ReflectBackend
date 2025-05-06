cc6cac8be9d667dbf6d32de636c232dec6d41c63
roman <r3wqaxz@yandex.ru>
2025-05-06 06:18:21 +0300

    Hotfix urls model

--- reflect_backend_app/urls.py
+++ reflect_backend_app/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/friends/', include('api.friends.urls')),
     path('api/ai/', include('api.ai.urls')),
-    path('api/profile/', include('api.profile.urls'))
+    path('api/profileReflect/', include('api.profileReflect.urls'))
 ]

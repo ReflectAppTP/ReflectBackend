ca6d46dc7134cf02fb4042aa1209f3ec086dae4a
roman <r3wqaxz@yandex.ru>
2025-05-19 00:10:14 +0300

    Update import

--- api/ai/consumers.py
+++ api/ai/consumers.py
@@ -6,6 +6,7 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reflect_backend_app.settings')
 django.setup()
+from api.ai.models import ChatMessage
 
 import json
 import pika

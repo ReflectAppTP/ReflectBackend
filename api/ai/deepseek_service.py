12f2da22458558325e67d15affc057e89e7aae3b
roman <r3wqaxz@yandex.ru>
2025-05-19 00:52:53 +0300

    Fix OPENROUTER_API_KEY

--- api/ai/deepseek_service.py
+++ api/ai/deepseek_service.py
@@ -7,7 +7,7 @@ class DeepSeekService:
     @staticmethod
     def get_response(messages):
         headers = {
-            "Authorization": f"Bearer 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",
+            "Authorization": f"Bearer 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",
             "HTTP-Referer": settings.OPENROUTER_REFERER_URL,
             "X-Title": settings.OPENROUTER_APP_NAME,
             "Content-Type": "application/json"

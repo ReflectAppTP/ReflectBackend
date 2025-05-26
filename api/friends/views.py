2d9adfa07e7395a11d7f1e80022a3249fe7e89b3
roman <r3wqaxz@yandex.ru>
2025-05-26 14:50:09 +0300

    Rename friends view

--- api/friends/views.py
+++ api/friends/views.py
@@ -89,7 +89,7 @@ class FriendshipViewSet(viewsets.ModelViewSet):
         )
 
     @action(detail=False, methods=['get'])
-    def list(self, request):
+    def listf(self, request):
         friendships = Friendship.objects.filter(
             Q(from_user=request.user) | Q(to_user=request.user),
             status=Friendship.ACCEPTED

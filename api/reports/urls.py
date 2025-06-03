f6d48c761db00572db6d587ab5ca7c97327716d7
roman <r3wqaxz@yandex.ru>
2025-06-03 10:37:18 +0300

    Fix import in urls

--- api/reports/urls.py
+++ api/reports/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import AdminReportViewSet, AdminUserUpdateView, AdminUserStateDetailView
+from .views import AdminReportViewSet, AdminUserStateDetailView
 from rest_framework.routers import DefaultRouter
 from .views import UserReportViewSet, StateReportViewSet, BlockUserView, AdminUserViewSet
 

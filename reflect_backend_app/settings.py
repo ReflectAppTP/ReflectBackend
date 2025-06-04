e8d310a1d3ebe4b7bb708b2c73a7056de8c7d5be
roman <r3wqaxz@yandex.ru>
2025-06-04 21:54:01 +0300

    Fix register access token requirement

--- reflect_backend_app/settings.py
+++ reflect_backend_app/settings.py
@@ -32,10 +32,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','185.185.71.233', 'reflect-app.ru', 'www.reflect-app.ru']
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://reflect-app.ru',
-    'https://www.reflect-app.ru',
-]
 
 # Application definition
 
